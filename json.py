ef3ebff53a5900318bfe283376bf7590f4fc4dc8
Roland Angerer <r.angerer@yelsterdigital.com>
2016-12-13 07:11:56 +0100

    add coding dojo outcome

--- json.py
+++ json.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def json():
-    pass
+def scalar_to_json(data):
+    if data is None:
+        return "null"
+    if isinstance(data, bool):
+        if data:
+            return 'true'
+        return 'false'
+    if isinstance(data, basestring):
+        return '"{}"'.format(data.replace('"', '\\"'))
+    return str(data)
+
+
+def json(data):
+    output = None
+    if isinstance(data, dict):
+        temp = ['"{}":{}'.format(k, json(v)) for k, v in sorted(data.items())]
+        output = ','.join(temp)
+        output = '{{{}}}'.format(output)
+    elif isinstance(data, list):
+        temp = [json(item) for item in data]            
+        output = ','.join(temp)
+        output = '[{}]'.format(output)
+    elif isinstance(data, (int, float, basestring, type(None), bool)):
+        output = scalar_to_json(data)
+    if output is None:
+        raise TypeError, "cannot convert this shit"
+    return output
+
 
 if __name__ == "__main__":
     import nose
--- test_json.py
+++ test_json.py
@@ -5,5 +5,89 @@ from nose.tools import *
 
 from json import json
 
-def test_json():
-    pass
+def test_json_basestring():
+    assert_equals(json("Hello World"), '"Hello World"')
+
+def test_json_integer():
+    assert_equals(json(9), "9")
+
+def test_json_float():
+    assert_equals(json(1.234), "1.234")
+    
+def test_json_array():
+    data = [1, 2, 3]
+    assert_equals(json(data), '[1,2,3]')
+
+def test_json_array02():
+    data = ['bla', 1, 1.2]
+    assert_equals(json(data), '["bla",1,1.2]')
+
+def test_json_dict():
+    data = { 'foo': 'bar' }
+    assert_equals(json(data), '{"foo":"bar"}')
+    
+def test_json_dict_list():
+    data = { 'foo': [1, 2, 3] }
+    assert_equals(json(data), '{"foo":[1,2,3]}')
+    
+def test_json_dict_int_key():
+    data = {1:[1, 2, 3] }
+    assert_equals(json(data), '{"1":[1,2,3]}')
+ 
+def test_json_dictindict():
+    data = { 'foo': {'fizz' : 'buzz'} }
+    assert_equals(json(data), '{"foo":{"fizz":"buzz"}}')
+
+def test_json_2_dict():
+    data = { 'foo': 'fizz', 'bar' : 'buzz'}
+    assert_equals(json(data), '{"bar":"buzz","foo":"fizz"}')
+
+def test_json_2_dict_2():
+    data = { 'foo': 'fizz', 'bar' : 'buzz', 'a': [1, 2, 3]}
+    assert_equals(json(data), '{"a":[1,2,3],"bar":"buzz","foo":"fizz"}')
+
+def test_empty_list():
+    data = []
+    assert_equals(json(data), "[]")
+    
+def test_empty_dict():
+    data = {}
+    assert_equals(json(data), "{}")
+    
+def test_list_with_empty_dict():
+    data = [{}]
+    assert_equals(json(data), "[{}]")
+    
+def test_rangie2():
+    data = {"": 0}
+    assert_equals(json(data), '{"":0}')
+    
+def test_none():
+    assert_equals(json(None), "null")
+
+def test_object():
+    def closure():
+        json(object())
+    assert_raises(TypeError, closure)
+    
+def test_bool():
+    assert_equals(json(True), 'true')
+    
+def test_object_in_array():
+    def closure():
+        json([object()])
+    assert_raises(TypeError, closure)
+
+def test_object_in_dict():
+    def closure():
+        json({'a': object()})
+    assert_raises(TypeError, closure)
+    
+def test_object_class():
+    def closure():
+        json(object)
+    assert_raises(TypeError, closure)
+
+def test_escape():
+    assert_equals(json('"') , '"\\""')
+        
